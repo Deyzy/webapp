022567f0e36fe25013b4fbfbd85e7c999488770f
deyzy <deisesantos487@gmail.com>
2025-01-28 08:13:48 -0300

    update

--- hello-world/src/rotas/loginUser.js
+++ hello-world/src/rotas/loginUser.js
@@ -1,41 +1,76 @@
 const createConnection = require('./db'); //função que cria a conexão
 
-// Função para processar o login
+// // Função para processar o login PRIMEIRO CODIGO
+// const loginUser = (req, res) => {
+//     const { username, password } = req.body;
+
+//     // Cria a conexão com o banco de dados chamando a função
+//     const connection = createConnection();
+
+//     const sql = 'SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1';
+
+//     // Verificar o usuário no banco de dados
+//     connection.query(
+//         sql, 
+//         [username, password],
+//         (err, results) => {
+//             connection.end(); // Fechar a conexão após a consulta
+
+//             if (err) {
+//                 return res.status(500).send('Erro na consulta ao banco de dados.');
+//             } 
+
+//             if (results.length > 0) {
+//                 const info = {
+//                     id: results[0].id,
+//                     username: results[0].username, 
+//                     fullname: results[0].fullname,
+//                     cpf: results[0].cpf,
+//                     phonenumber: results[0].phonenumber,
+//                     address: results[0].address,
+//                     dateofbirth: results[0].dateofbirth,
+//                 }
+//                 return res.render('info', { info });
+//             } else {
+//                 return res.send('Usuário ou senha incorretos.');
+//             }
+//         }
+//     );
+// };
+
+// module.exports = { loginUser };
+
+
+
 const loginUser = (req, res) => {
     const { username, password } = req.body;
-
-    // Cria a conexão com o banco de dados chamando a função
     const connection = createConnection();
 
     const sql = 'SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1';
 
-    // Verificar o usuário no banco de dados
     connection.query(
         sql, 
         [username, password],
         (err, results) => {
-            connection.end(); // Fechar a conexão após a consulta
+            connection.end();
 
             if (err) {
                 return res.status(500).send('Erro na consulta ao banco de dados.');
-            } 
+            }
 
             if (results.length > 0) {
-                const info = {
-                    id: results[0].id,
-                    username: results[0].username, 
-                    fullname: results[0].fullname,
-                    cpf: results[0].cpf,
-                    phonenumber: results[0].phonenumber,
-                    address: results[0].address,
-                    dateofbirth: results[0].dateofbirth,
+                
+                const user = results[0];
+                if (user.adm === 1) { // Se for admin
+                    // Passar a informação do usuário logado e redirecionar para o painel
+                    return res.render('admin', { user });
+                } else {
+                    // Usuário comum
+                    return res.render('info', { info: user });
                 }
-                return res.render('info', { info });
             } else {
                 return res.send('Usuário ou senha incorretos.');
             }
         }
     );
 };
-
-module.exports = { loginUser };
--- hello-world/src/server.js
+++ hello-world/src/server.js
@@ -6,7 +6,6 @@ const {login} = require('./rotas/login')
 const createConnection = require('./rotas/db');
 const {info} = require('./rotas/info')
 const {loginUser} = require('./rotas/loginUser');
-const { getUsers } = require('./rotas/crud');
  
 //CRIAR UMA PAG ADMIN, COM UMA PALAVRA SECRETA QUE SÓ O ADM TEM ACESSO
  
@@ -24,20 +23,6 @@ const connection = createConnection(); //função para conexão com o MySQL
 
 app.get('/', login ); // para chamar a página de login
 
-// Rota para informações dos usuários (listar usuários)
-app.get('/usuarios', (req, res) => {
-    connection.query('SELECT * FROM users', (err, result) => {
-        if (err) {
-            console.error('Erro ao buscar usuários:', err);
-            res.sendStatus(500);
-            return;
-        }
-        console.log(result);
-        res.render('usuarios', { result }); 
-    });
-});
-
-
 app.get('/info', info); 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
@@ -47,34 +32,131 @@ app.post('/login', (req, res) => {
 
     const sql = 'SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1';
 
+//     // Verificar o usuário no banco de dados
+//     connection.query(
+//         sql, 
+//         [username, password],
+//         (err, results) => {
+//             connection.end(); // Fechar a conexão após a consulta
+
+//             if (err) {
+//                 return res.status(500).send('Erro na consulta ao banco de dados.');
+//             } 
+
+//             if (results.length > 0) {
+//                 if (results[0].adm === 1) {
+//                     const users = getUsers()
+//                     const info = {
+//                         users, results
+//                     }
+//                     console.log(info)
+//                     return res.render('usuarios', {info});
+//                 } else {
+//                     return res.render('info', { info:results[0]});
+//                 }
+//             } else {
+//                 return res.send('Usuário ou senha incorretos.');
+//             }
+//         }
+//     );
+// }); 
+
+
     // Verificar o usuário no banco de dados
     connection.query(
-        sql, 
+        sql,
         [username, password],
         (err, results) => {
-            connection.end(); // Fechar a conexão após a consulta
-
             if (err) {
+                connection.end(); // Fechar a conexão se der erro
                 return res.status(500).send('Erro na consulta ao banco de dados.');
-            } 
+            }
 
             if (results.length > 0) {
                 if (results[0].adm === 1) {
-                    const users = getUsers()
-                    const info = {
-                        users, results
-                    }
-                    console.log(info)
-                   // return res.render('usuarios', {info});
+                    // Se for admin, buscar todos os usuários
+                    connection.query('SELECT * FROM users', (err, users) => {
+                        if (err) {
+                            connection.end(); // Fechar a conexão se der erro
+                            return res.status(500).send('Erro ao buscar usuários');
+                        }
+                        const info = {
+                            users,
+                            results
+                        };
+                        // console.log(info);
+                         res.render('admin', { user: results[0], usuarios: users });
+                    });
                 } else {
-                    return res.render('info', { info:results[0]});
+                    // Se não for admin, renderiza a página de informações do usuário
+                    connection.end(); // Fechar a conexão após finalizar todas as operações
+                    return res.render('info', { info: results[0] });
                 }
             } else {
+                connection.end(); // Fechar a conexão se não encontrar o usuário
                 return res.send('Usuário ou senha incorretos.');
             }
         }
     );
-}); 
+});
+
+
+// Rota para o painel de administração
+app.get('/admin', (req, res) => {
+    const connection = createConnection();
+    connection.query('SELECT * FROM users', (err, users) => {
+        if (err) {
+            console.error('Erro ao buscar usuários:', err);
+            return res.status(500).send('Erro ao buscar usuários');
+        }
+        res.render('admin', { users });
+    });
+});
+
+// Rota para excluir um usuário
+app.get('/usuarios/excluir/:id', (req, res) => {
+    const { id } = req.params;
+    const connection = createConnection();
+    
+    connection.query('DELETE FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao excluir usuário:', err);
+            return res.status(500).send('Erro ao excluir usuário');
+        }
+        res.redirect('/admin');  // Redireciona para o painel de administração
+    });
+});
+
+// Rota para editar um usuário
+app.get('/usuarios/editar/:id', (req, res) => {
+    const { id } = req.params;
+    const connection = createConnection();
+    
+    connection.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar usuário:', err);
+            return res.status(500).send('Erro ao buscar usuário');
+        }
+        res.render('editarUsuario', { usuario: results[0] });
+    });
+});
+
+app.post('/usuarios/editar/:id', (req, res) => {
+    const { id } = req.params;
+    const { fullname, cpf, phonenumber, address, dateofbirth } = req.body;
+    const connection = createConnection();
+    
+    const query = 'UPDATE users SET fullname = ?, cpf = ?, phonenumber = ?, address = ?, dateofbirth = ? WHERE id = ?';
+    connection.query(query, [fullname, cpf, phonenumber, address, dateofbirth, id], (err, results) => {
+        if (err) {
+            console.error('Erro ao atualizar usuário:', err);
+            return res.status(500).send('Erro ao atualizar usuário');
+        }
+        res.redirect('/admin');  // Redireciona para o painel de administração
+    });
+});
+
+
 
 
 // Inicia o servidor
